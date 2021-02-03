811b48b402da7ff7e3daca6cf336252e54afb156
kamaljeet <kamaljeet.singh10@wipro.com>
2021-02-03 20:38:35 +0530

    Deployment config updated for cluster name

--- terraform/GCP/GKE/gcp-gke-pvt.tf
+++ terraform/GCP/GKE/gcp-gke-pvt.tf
@@ -20,6 +20,29 @@ resource "google_compute_subnetwork"  "primary_subnet" {
     } ]
 }
 
+resource "google_compute_router" "gke-router" {
+  name    = "gke-router"
+  region  = google_compute_subnetwork.primary_subnet.region
+  network = google_compute_network.vpc_network.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "gke-nat" {
+  name                               = "gke-nat"
+  router                             = google_compute_router.gke-router.name
+  region                             = google_compute_router.gke-router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
 resource "google_service_account" "gke_default" {
     account_id = "gke-serviceaccount"
     display_name = "gke-serviceaccount"
